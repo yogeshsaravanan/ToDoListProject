026a2c9d738d69eb0587fee088c6af0b26396f69
yogeshsaravanan <53184345+yogeshsaravanan@users.noreply.github.com>
2024-02-24 22:10:58 +0530

    Update models.py

--- CheckListAPP/models.py
+++ CheckListAPP/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class CheckList(models.Model):
-    # manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     task = models.CharField(max_length=300)
     start_time= models.CharField(max_length=10)
     end_time = models.CharField(max_length=10)
