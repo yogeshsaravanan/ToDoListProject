51301359eebd0cd34ec97d7c49c4956940ccb0d3
Yogeshwaran Saravanan <53184345+yogeshsaravanan@users.noreply.github.com>
2024-04-04 22:58:03 +0530

    Update models.py

--- ChatBot/models.py
+++ ChatBot/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 
 class ChatBot(models.Model):
     manage = models.ForeignKey(User,on_delete=models.CASCADE,  default=None)
